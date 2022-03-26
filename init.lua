b4d3d83a490af6c836ca1903228a24840ecae777
James Storm <jamesstorm@gmail.com>
2022-03-26 12:56:21 -0400

    aerial symbol thingy

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 vim.g.mapleader = " "
-
 vim.opt.relativenumber = true
 vim.opt.errorbells = false
 vim.opt.shiftwidth = 4
@@ -11,7 +10,7 @@ vim.opt.smartcase = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.undodir = "/home/james/.config/nvim/undodir"
+vim.opt.undodir = os.getenv( "HOME" ) .. '/undodir'
 vim.opt.incsearch = true
 vim.opt.colorcolumn = "80"
 vim.cmd("highlight ColorColumn ctermbg=0 guibg=lightgrey")
@@ -21,44 +20,49 @@ vim.cmd("set guicursor=n-v-c:block-Cursor")
 vim.cmd("set guicursor+=i:ver100-iCursor")
 vim.cmd("set guicursor+=n-v-c:blinkon0")
 vim.cmd("set guicursor+=i:blinkwait10")
---
+
+
+
+
 require('packer').startup(function()
-use 'wbthomason/packer.nvim'
-use 'neovim/nvim-lspconfig'
-use 'hrsh7th/nvim-cmp'
-use 'hrsh7th/cmp-nvim-lsp'
-use 'L3MON4D3/LuaSnip'
-use 'saadparwaiz1/cmp_luasnip'
-use 'onsails/lspkind-nvim'
---use 'vim-airline/vim-airline'
---use 'vim-airline/vim-airline-themes'
-use 'nvim-treesitter/nvim-treesitter'
-use 'nvim-lua/plenary.nvim'
-use 'lewis6991/gitsigns.nvim'
-use 'nvim-telescope/telescope.nvim'
-use {
-  'nvim-telescope/telescope.nvim',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-}
-use 'preservim/nerdtree'
-use 'ThePrimeagen/harpoon'
+--    use { 'morhetz/gruvbox' }
+    use 'stevearc/aerial.nvim'
+    use 'EdenEast/nightfox.nvim'
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+    use 'onsails/lspkind-nvim'
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'nvim-lua/plenary.nvim'
+    use 'lewis6991/gitsigns.nvim'
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'preservim/nerdtree'
+    use 'ThePrimeagen/harpoon'
 end)
 
 require('jamesstorm/lsp')
 require('jamesstorm/treesitter')
 require('jamesstorm/gitsigns')
 require('jamesstorm/lualine')
+require('jamesstorm/aerial')
 
-local function map(mode, lhs, rhs, opts)
-  local options = {noremap = true}
-  if opts then options = vim.tbl_extend('force', options, opts) end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {})
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {})
+
+
+--vim.opt.termguicolors = true
+--vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme nightfox]])
+--vim.cmd([[colorscheme gruvbox]])
--- lua/jamesstorm/lsp.lua
+++ lua/jamesstorm/lsp.lua
@@ -1,6 +1,6 @@
 -- lspconfig
 local nvim_lsp = require('lspconfig')
-local servers = { 'jedi_language_server' }
+local servers = { 'jedi_language_server', 'rust_analyzer' }
 
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -23,6 +23,9 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
     buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
     buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+    require("aerial").on_attach(client, bufnr)
+
 end
 
 
