d0a1f5c467a2c06a5005da1fc8bbf66f5408aa45
James Storm <jamesstorm@gmail.com>
2023-03-28 10:09:47 +0000

    too many telescopes

--- lua/jamesstorm/packer.lua
+++ lua/jamesstorm/packer.lua
@@ -2,11 +2,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
 
   use {
     "folke/trouble.nvim",
