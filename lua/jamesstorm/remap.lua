6dbabd83bf94089aa1559654029732c48505fe1e
James Storm <jamesstorm@gmail.com>
2023-03-28 08:06:32 +0000

    remap for the pacler dance

--- lua/jamesstorm/remap.lua
+++ lua/jamesstorm/remap.lua
@@ -41,3 +41,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>sps", "<cmd>source ~/.config/nvim/lua/jamesstorm/packer.lua<CR><cmd>PackerSync<CR>q<cmd>lua ColorMyPencils()<CR>")
