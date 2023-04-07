a0bf4e514bd9ad1a5b1cc8633fd2ab8319097bd0
James Storm <jamesstorm@gmail.com>
2023-04-07 15:39:36 -0400

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,14 +2,14 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+-- lsp.configure('lua-language-server', {
+--     settings = {
+--        Lua = {
+--            diagnostics = {
+--                globals = { 'vim' }
+--            }
+--        }
+--    }
+--})
 
 lsp.setup()
